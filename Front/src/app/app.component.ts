4f22dbc2c8f8fb04bbe2912542f8d269c1fd60e5
mouadhfarhat <fmouadh50@gmail.com>
2024-08-06 15:49:54 +0100

    majMouadh

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./layout/header/header.component";
 import {ChartComponent} from "./layout/chart/chart.component";
-
+import { FooterComponent } from './layout/footer/footer.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ChartComponent],
+  imports: [RouterOutlet, HeaderComponent, ChartComponent, FooterComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
