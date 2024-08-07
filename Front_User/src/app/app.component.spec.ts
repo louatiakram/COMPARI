5e2085f8366500c61183e188b74789af846791d2
mouadhfarhat <fmouadh50@gmail.com>
2024-08-07 16:31:39 +0100

    maj

--- Front_User/src/app/app.component.spec.ts
+++ Front_User/src/app/app.component.spec.ts
@@ -1,11 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [AppComponent]
   }));
 
   it('should create the app', () => {
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Front_User'`, () => {
+  it(`should have the 'Front-User' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Front_User');
+    expect(app.title).toEqual('Front-User');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Front_User app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Front-User app is running!');
   });
 });
--- Front_User/src/app/app.component.ts
+++ Front_User/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductsComponent } from './components/products/products.component';
-import { BillboardComponent } from './components/billboard/billboard.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchComponent, ProductsComponent, BillboardComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Front_User';
+  title = 'Front-User';
 }
