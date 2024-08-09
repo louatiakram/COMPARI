364db41352c3a9c2ebfb048e362ca461ad9c2cd5
mouadhfarhat <fmouadh50@gmail.com>
2024-08-09 15:25:16 +0100

    maj9/8/2024-2

--- Front_User/src/app/app.component.ts
+++ Front_User/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BillboardComponent } from './components/billboard/billboard.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BillboardComponent, HeaderComponent, SearchComponent, ProductsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
