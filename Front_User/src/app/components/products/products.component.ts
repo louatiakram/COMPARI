99cbfec95f1eba226e2012e4d6a88d4a3872421a
mouadhfarhat <fmouadh50@gmail.com>
2024-08-09 15:06:24 +0100

    maj9/8/2024

---
+++ Front_User/src/app/components/products/products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+
+}
