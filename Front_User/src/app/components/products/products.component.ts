8b6b2855c7f8898f45ab2d7443a26c4cd7c2a6c4
mouadhfarhat <fmouadh50@gmail.com>
2024-08-12 09:29:11 +0100

    maj(singleproductComponent)

--- Front_User/src/app/components/products/products.component.ts
+++ Front_User/src/app/components/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SingleProductComponent } from '../single-product/single-product.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SingleProductComponent],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
