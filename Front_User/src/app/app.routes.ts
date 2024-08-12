1c4474df1c2ca5e85cc834b600dea1e17c2c4871
mouadhfarhat <fmouadh50@gmail.com>
2024-08-12 16:03:18 +0100

    maj

--- Front_User/src/app/app.routes.ts
+++ Front_User/src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    {path: "", component: HomeComponent },
     {path: "login", component: LoginComponent},
     {path: "signup", component: SignupComponent},
-    {path: "", component: HomeComponent }
+    
 ];
