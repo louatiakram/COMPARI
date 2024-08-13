901238732f92b3a27ec0662120c2b1bc80c89049
mouadhfarhat <fmouadh50@gmail.com>
2024-08-13 14:35:16 +0100

    maj

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,4 +1,8 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
+import { TableUserComponent } from './layout/table-user/table-user.component';
 
-export const routes: Routes = [ {path: "", component: HomeComponent },];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'userList', component: TableUserComponent },
+];
