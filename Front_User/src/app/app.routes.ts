3e9ababfdf6456f553b18161ba7270cf8d5c061c
mouadhfarhat <fmouadh50@gmail.com>
2024-08-20 12:02:34 +0100

    maj

--- Front_User/src/app/app.routes.ts
+++ Front_User/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {SignupComponent} from './components/signup/signup.component';
 import {HomeComponent} from './components/home/home.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
 
 export const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
   {path: "signup", component: SignupComponent},
+  {path: "wishlist", component: WishlistComponent},
 
 ];
---
+++ Front_User/src/app/components/wishlist/wishlist.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-wishlist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wishlist.component.html',
+  styleUrls: ['./wishlist.component.scss']
+})
+export class WishlistComponent {
+
+}
